4955c843617d580b46184f13825102e5fd0425b8
Dmitry Giannopulos <noc@ioli.be>
2015-12-13 17:01:53 +0200

    Update anchorCache.php

--- anchorCache/anchorCache.php
+++ anchorCache/anchorCache.php
@@ -1,13 +1,34 @@
 <!-- start > iOli® Cloud / Moscow, Russia https://ioli.be <! Close -->
 
 <?php
+
   /* Assign your dynamically generated page to $page */
   $page = "anchorCache.php";
+  
+  /*Extension to give cached files (usually cache, htm, txt) */
+  $cacheext = 'html';
+  
   /* Define path and name of cached file */
   $cachefile = 'anchorCache/' . md5('$page');
  
   /* How long to keep cache file? */
   $cachetime = 1800;
+  
+  
+  /* Ignore List */
+  $ignore_list = array(
+  '/info.php',
+  '/project/'
+  );
+  
+  /* Script */
+  /* Requested page */
+  $page = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  
+  /* Cache file to either load or create */
+  $cachefile = $cachedir . md5($page) . '.' . $cacheext;
+  $cachefile_created = ((@file_exists($cachefile)) and ($ignore_page === false)) ? @filemtime($cachefile) : 0;
+  @clearstatcache();
  
   /* Is cache file still fresh? If so, serve it */
  
@@ -20,11 +41,12 @@
   ob_start();
 ?>
 
-<?php   
+<?php
+   
  /* Save the cached content to a file */   
  $fp = fopen($cachefile, 'w');    
  fwrite($fp, ob_get_contents());    
- fclose($fp);    
+ fclose($fp);
  
  /* Send browser output */   
  ob_end_flush();
